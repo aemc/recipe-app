a81c9d3ea8e396566dd48fd2d6ea16f1b6a4251a
Ammiel <ammiel@umapped.com>
2018-12-28 21:49:00 -0500

    Remove redundant files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import "./App.css";
 import Dashboard from "../Dashboard/Dashboard";
 import { Redirect } from "react-router";
+
+import { withStyles } from "@material-ui/core";
+
+const styles = theme => ({
+  textAlign: "center"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
         <BrowserRouter>
           <Switch>
             <Route
@@ -25,4 +30,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { jss, JssProvider } from "react-jss";
 
-import "./index.css";
 import App from "./components/App/App";
 import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 import { theme } from "./config/muiTheme";
 
-import { createGenerateClassName } from "@material-ui/core/styles";
-
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { createGenerateClassName } from "@material-ui/core/styles";
 
 const generateClassName = createGenerateClassName();
 
